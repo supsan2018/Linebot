8ff4d28dee8aad6a8514afb5c0e9cac7ad2b930a
arpo9999 <39718491+arpo9999@users.noreply.github.com>
2018-06-03 12:42:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -55,8 +55,8 @@ if (!is_null($events['events'])) {
 			if($text == "ภาพ"){
 				$messages = [
 				'type' => 'image',
-				'originalContentUrl' => "https://paaying.files.wordpress.com/2008/06/e0b881e0b8a5e0b989e0b8a7e0b8a2.jpg",
-    				'previewImageUrl' => "https://paaying.files.wordpress.com/2008/06/e0b881e0b8a5e0b989e0b8a7e0b8a2.jpg"
+				'originalContentUrl' => "https://www.picz.in.th/images/2018/06/03/zeYd6u.jpg",
+    				'previewImageUrl' => "https://www.picz.in.th/images/2018/06/03/zeYd6u.jpg"
 			];
 			}
 			/*if($text == "image"){
